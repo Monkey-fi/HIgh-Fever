507d3dde8159ad24e7978d0b3923aa94c4653690
Monkey-fi <sparsh2312049gmail.com>
2025-01-11 23:05:39 +0530

    before animating player movement

---
+++ Assets/Script/InputManager.cs
@@ -0,0 +1,43 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class InputManager : MonoBehaviour
+{
+    PlayerControls playerControls; // class generated is referenced here as variable
+
+    public Vector2 movementInput;
+    public float verticalInput;
+    public float horizontalInput;
+
+    private void OnEnable()
+    {
+        if (playerControls == null)
+        {
+            playerControls = new PlayerControls();
+            playerControls.PlayerMovement.Movement.performed += i => movementInput = i.ReadValue<Vector2>();
+        }
+        playerControls.Enable();
+    }
+
+    private void OnDisable()
+    {
+        playerControls.Disable();
+    }
+
+    public void HandleAllInputs()
+    {
+        HandleMovementInput();
+        //handlejumpinginput and action input
+    }
+    
+    private void HandleMovementInput()
+    {
+        verticalInput = movementInput.y;
+        horizontalInput = movementInput.x;
+    }
+    //private void Update()
+    //{
+    //    Debug.Log($"Movement Input: {movementInput}");
+    //}
+}
